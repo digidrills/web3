521022e54bdcf0c02fb3e9d41c47995cff35555f
Hariharan R <r.hariharan54@gmail.com>
2022-06-17 00:32:57 +0530

    ERC721 based asset stroage contract update

--- solidity-samples/NFTContract.sol
+++ solidity-samples/NFTContract.sol
@@ -13,23 +13,22 @@ contract NFTContract is ERC721URIStorage {
 
     constructor() ERC721("Property", "PRT") {}
 
-    function awardItem(address player,string memory name,string memory latitude,string memory longitude,string memory url)
+    function mint_and_transfer(address user_address,string memory name,string memory latitude,string memory longitude,string memory url)
         public
         returns (uint256)
     {
         uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-        
+        _mint user_address, newItemId);
+        // creating a JSON object with 4 fields name,latitude,longitude and url
         bytes memory dataURI = abi.encodePacked(
             '{',
                 '"name": "', name, '",',
                 '"latitude": "', latitude, '",',
                 '"longitude": "', longitude, '",',
                 '"url": "', url, '"',
-                //  additional Metadata properties as required
             '}'
         );
-
+        //the uri field contains the base64 encoded string of the above json , which is stored on chain
         _setTokenURI(newItemId,string(
             abi.encodePacked(
                 "data:application/json;base64,",
